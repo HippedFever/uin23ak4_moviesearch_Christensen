631f320b9e627bda563d4bd0521322c651c3a970
HippedFever <Matsc99@gmail.com>
2023-03-30 05:09:24 +0200

    Fungerende kode, klar for merge til main

--- filmdatabase/README.md
+++ filmdatabase/README.md
@@ -1,4 +1,13 @@
-Filmdatabase nettside laget av Mats Christensen
+# Filmdatabase nettside laget av Mats Christensen
 
-Kilder:
-https://www.omdbapi.com/
+## Kilder & Inspirasjon:
+
+1. https://www.omdbapi.com/
+2. https://www.bestcssbuttongenerator.com/#/30
+3. https://www.w3schools.com/
+4. https://medium.com/nerd-for-tech/the-open-movie-database-omdb-for-fetch-movie-data-bc5ff46bec8
+5. https://stackoverflow.com/
+6. https://place-hold.it/
+7. https://react.dev/learn
+8. https://sass-lang.com/documentation/
+9. Tidligere forelesningsoppgaver
--- filmdatabase/src/App.js
+++ filmdatabase/src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
 import './css/main.css'
 import Moviecard from './components/moviecard';
-import Searchresult from './components/searchresults';
+import SearchResult from './components/searchresults';
+import React, { useState } from 'react';
 
 
 
 function App() {
+  const [query, setQuery] = useState('James Bond');
+  
+  const handleSearch = (query) => {
+    setQuery(query)
+  }
+  
+
   return (
     <body>
       <header>
         <h1 className="maintitle">Filmsøk</h1>
-        <h5>SØK HER</h5>
+        <SearchResult onSearch={handleSearch} />
         </header>
-        <Moviecard />
+        <Moviecard query={query} />
     </body>
   );
 }
